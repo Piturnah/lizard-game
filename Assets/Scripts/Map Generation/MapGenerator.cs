61a0ea60dd60b1e8ba192fa5909b7bcd13259ba3
Piturnah <peterhebden6@gmail.com>
2020-03-18 16:45:33 +0000

    Island Gen + Water Shader

---
+++ Assets/Scripts/Map Generation/MapGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public enum DrawMode {NoiseMap, Mesh, FalloffMap};
+    public DrawMode drawMode;
+
+    const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail;
+    public float noiseScale;
+
+    public int octaves;
+
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public string worldSeed;
+
+    public bool useFalloff;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public bool autoUpdate;
+
+    float[,] falloffMap;
+
+    private void Awake() {
+        falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
+    }
+
+    public void GenerateMap() {
+        int seed = (worldSeed != "") ? ((int.TryParse(worldSeed, out int i))? i : worldSeed.GetHashCode()) : Random.Range(int.MinValue, int.MaxValue);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity);
+        Debug.Log("World generated with seed: " + seed.ToString());
+        if (useFalloff) {
+            for (int y = 0; y < mapChunkSize; y++) {
+                for (int x = 0; x < mapChunkSize; x++) {
+                    noiseMap[x, y] = Mathf.Clamp01(noiseMap[x, y] - falloffMap[x, y]);
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawNoiseMap(noiseMap);
+        }
+        else if (drawMode == DrawMode.Mesh) {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail, true));
+        }
+        else if (drawMode == DrawMode.FalloffMap) {
+            display.DrawNoiseMap(FalloffGenerator.GenerateFalloffMap(mapChunkSize));
+        }
+    }
+
+    private void OnValidate() {
+        if (lacunarity < 1) {
+            lacunarity = 1;
+        }
+        if (octaves < 0) {
+            octaves = 0;
+        }
+
+        falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
+    }
+}
